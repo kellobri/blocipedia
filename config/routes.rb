f999611bfafa43d0c462a90e0ec3cbf5389726a5
Kelly O'Briant <kellyobriant@gmail.com>
2015-03-28 18:29:09 -0400

    authentication from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'logout', to:'sessions#destroy', as: 'logout'
+  get 'signin', to: 'sessions#new', as: 'signin'
+  get 'signup', to: 'users#new', as: 'signup'
+  resources :users
+  resources :sessions
 
+  get 'welcome/index'
   root to: 'welcome#index'
 end
